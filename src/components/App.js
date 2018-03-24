3066536f23517d8068bd2e5ac32f426314da1b83
Nacho Villaverde Montalvo <nachovillaverde1@gmail.com>
2018-03-24 23:18:04 +0100

    make test for fetch

--- src/components/App.js
+++ src/components/App.js
@@ -11,36 +11,43 @@ class App extends Component {
 
   constructor(props){
     super(props)
-    this.state = {jackpots:[]}
+    this.state = {jackpots:{}}
   }
   componentWillMount(){
-    var url = "https://www.lottoland.com/api/drawings/euroJackpot?last";
- 
-    fetch(url).then((data)=>{
+    var url = "https://www.lottoland.com/api/drawings/euroJackpot";
 
-      return data.json()
-    })
-    .then((jackpots)=>{
-      //console.log(jackpots.last.numbers);
-      this.setState({ jackpots: jackpots })
-      console.log(jackpots);
-      //console.log(jackpots.last.numbers);
-      //for date
-      // let date =document.getElementById("DateBtn");
-      // date.innerHTML= `${jackpots.last.date.dayOfWeek}, ${jackpots.last.date.day}/${jackpots.last.date.month}/${jackpots.last.date.year}`;
-      // let subTtl =document.getElementById("subtileRes");
-      // subTtl.innerHTML= `EuroJackpot Results for ${jackpots.last.date.dayOfWeek}, ${jackpots.last.date.day}/${jackpots.last.date.month}/${jackpots.last.date.year}`;
-    });
-  
+    fetch(url)
+    .then(data=>data.json())
+    .then(jackpots => this.setState({jackpots}));
+    //.then(data=>data.map(numbers=))
+ 
+    // fetch(url).then((data)=>{ data.json()
+    // })
+    // .then((jackpots)=>{
+    //   //console.log(jackpots.last.numbers);
+    //   this.setState({ jackpots: jackpots })
+    //   console.log(jackpots.last.numbers);
+    //   //console.log(jackpots.last.numbers);
+    //   //for date
+    //   // let date =document.getElementById("DateBtn");
+    //   // date.innerHTML= `${jackpots.last.date.dayOfWeek}, ${jackpots.last.date.day}/${jackpots.last.date.month}/${jackpots.last.date.year}`;
+    //   // let subTtl =document.getElementById("subtileRes");
+    //   // subTtl.innerHTML= `EuroJackpot Results for ${jackpots.last.date.dayOfWeek}, ${jackpots.last.date.day}/${jackpots.last.date.month}/${jackpots.last.date.year}`;
+    // });
   }
   render() {
-    
+    var last = JSON.stringify(this.state.jackpots.last);
+    console.log(last);
+
     return(
       <div>
         <Header/>
         <JackpotNav/>
         <SecondaryNav/>
         <div className="container">
+        
+         
+          <h4>{last}</h4>
           <Results/>
           <div className="row">
             <JackpotList/>
--- src/components/Results.js
+++ src/components/Results.js
@@ -6,6 +6,7 @@ class Results extends Component {
       return(
         <div>
           <h5><strong>EUROJACKPOT RESULTS & WINNING NUMBERS</strong></h5> 
+
           <div id="DateBtn">Fecha</div>
           <hr/> 
           <h6 id="subtileRes">EuroJackpot Results for</h6> 
